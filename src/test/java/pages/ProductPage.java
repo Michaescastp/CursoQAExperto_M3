0e17e83f0b3da8e6c12d34bfa09cdae735cad4ec
Rose <rose@gmail.com>
2024-08-24 14:00:17 -0300

    Actualización dos commits

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductPage {
+	// Navegador
     private WebDriver driver;
 
     private By addToCartButton = By.linkText("Add to cart");
